94c63f0d495a141d24c59315475fd237b6f0b4d7
Villyan Sutanto <villyansutanto@gmail.com>
2025-12-19 13:45:32 +0700

    Prepare GIS web proxy for deployment

--- app.py
+++ app.py
@@ -84,4 +84,6 @@ def generate_fallback():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
